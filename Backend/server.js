5f32c30b08457e17dab75594327745be8d69c969
Aditya3034 <aditya3034@gmail.com>
2024-09-22 17:05:22 +0530

    fourth commit

--- Backend/server.js
+++ Backend/server.js
@@ -12,6 +12,16 @@ const Product = require("./models/Product");
 const app = express();
 const PORT = 4000;
 main();
+
+
+// CORS middleware
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // Allow all origins
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 app.use(express.json());
 app.use(cors(
   {origin: ["https://ims-api-ten.vercel.app"],
